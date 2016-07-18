c84a1d30d7e6c5221fec894eea6d495366b00e75
jirolabo <jirolabo@outlook.com>
2016-07-18 13:14:33 +0900

    StudentList を追加

---
+++ 1-Iterator/Iterator/StudentList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class StudentList
+    {
+        protected Student[] students;
+        private int last = 0;
+
+        public StudentList() { }
+        public StudentList(int studentCount)
+        {
+            this.students = new Student[studentCount];
+        }
+
+        public void add(Student student)
+        {
+            students[last] = student;
+            last++;
+        }
+        public Student getStudentAt(int index)
+        {
+            return students[index];
+        }
+        public int getLastNum()
+        {
+            return last;
+        }
+    }
+}
