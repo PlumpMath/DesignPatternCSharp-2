7ebd2e1d46b676780516144fb370e26cca830f82
jirolabo <jirolabo@outlook.com>
2016-07-18 13:09:22 +0900

    初回コミット

---
+++ 1-Iterator/Iterator/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class Student
+    {
+        private String name;
+        private int sex; //男の子:1  女の子:2
+
+        public Student(String name, int sex)
+        {
+            this.name = name;
+            this.sex = sex;
+        }
+        public String getName()
+        {
+            return name;
+        }
+        public int getSex()
+        {
+            return sex;
+        }
+    }
+}
