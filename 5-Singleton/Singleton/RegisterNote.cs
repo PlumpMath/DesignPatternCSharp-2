bd0689253e6cbf3034d9fd20695fc0865b1b27bb
jirolabo <jirolabo@outlook.com>
2016-07-23 09:32:19 +0900

    Singleton プロジェクト作成

---
+++ 5-Singleton/Singleton/RegisterNote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class RegisterNote
+    {
+        private static RegisterNote registerNote = new RegisterNote();
+        private RegisterNote() { }
+        public static RegisterNote getInstance()
+        {
+            return registerNote;
+        }
+    }
+}
